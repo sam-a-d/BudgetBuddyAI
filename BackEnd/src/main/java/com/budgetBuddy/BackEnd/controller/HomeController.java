6c9d5ea9e9dd796b6177bd4b00b33abd38528dac
sam-a-d <masamad.sust@gmail.com>
2025-02-17 23:58:27 +0200

    get rid of authorization check for home controller

--- BackEnd/src/main/java/com/budgetBuddy/BackEnd/controller/HomeController.java
+++ BackEnd/src/main/java/com/budgetBuddy/BackEnd/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.budgetBuddy.BackEnd.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @PreAuthorize("PermitAll()")
     @GetMapping("/")
     public String homePage(){
         return "Budget Buddy - A Personal Budget Tracker V.0.0.1";
