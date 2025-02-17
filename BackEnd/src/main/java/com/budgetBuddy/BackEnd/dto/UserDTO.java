eef0af440b6812aa91ae41adb614c95c99e8cdca
sam-a-d <masamad.sust@gmail.com>
2025-02-17 23:57:49 +0200

    aded user dto

---
+++ BackEnd/src/main/java/com/budgetBuddy/BackEnd/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.budgetBuddy.BackEnd.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+    private String username;
+    private String password;
+    private String role; // Expected: "ROLE_ADMIN" or "ROLE_USER"
+}
