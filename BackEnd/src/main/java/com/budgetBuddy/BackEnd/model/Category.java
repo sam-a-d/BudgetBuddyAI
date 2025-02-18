f7df12a63a34ad1c33d1d59f1a0e9480d84899c3
sam-a-d <masamad.sust@gmail.com>
2025-02-18 21:13:35 +0200

    aded constructor manually in category

--- BackEnd/src/main/java/com/budgetBuddy/BackEnd/model/Category.java
+++ BackEnd/src/main/java/com/budgetBuddy/BackEnd/model/Category.java
@@ -1,22 +1,27 @@
 package com.budgetBuddy.BackEnd.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Entity
 @Getter
 @Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(unique = true)
     private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
 }
