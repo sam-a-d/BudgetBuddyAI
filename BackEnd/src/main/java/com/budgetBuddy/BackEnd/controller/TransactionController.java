cd19dc5d61c79ea524a27652aec89e8a144073b2
sam-a-d <masamad.sust@gmail.com>
2025-02-20 00:29:49 +0200

    CORS added for transaction

--- BackEnd/src/main/java/com/budgetBuddy/BackEnd/controller/TransactionController.java
+++ BackEnd/src/main/java/com/budgetBuddy/BackEnd/controller/TransactionController.java
@@ -5,14 +5,12 @@ import com.budgetBuddy.BackEnd.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class TransactionController {
 
     @Autowired
