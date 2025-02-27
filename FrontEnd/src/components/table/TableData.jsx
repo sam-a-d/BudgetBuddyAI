01297c6c6ffc3113e28d72afccedf242ebf38ebe
sam-a-d <masamad.sust@gmail.com>
2025-02-27 20:48:42 +0200

    table row and table cell data component aded

---
+++ FrontEnd/src/components/table/TableData.jsx
@@ -0,0 +1,8 @@
+export default function TableData(data) {
+    return(
+        <td className="border border-gray-300 px-4 py-2">
+            {data.data}
+        </td>
+    )    
+    
+}
---
+++ FrontEnd/src/components/table/TableRow.jsx
@@ -0,0 +1,9 @@
+import TableData from "./TableData"
+
+export default function TableRow(data){
+    return (
+        <tr>
+            <TableData data={data}/>
+        </tr>
+    ) 
+}
