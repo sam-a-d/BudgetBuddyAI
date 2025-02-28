ce4ce59a7d6fb6027d75d39d6c9aa2b47e56424f
sam-a-d <masamad.sust@gmail.com>
2025-02-28 15:31:29 +0200

    removed some components from index

--- FrontEnd/src/index.jsx
+++ FrontEnd/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './components/Navbar'
+import NavSidebar from './components/NavSidebar'
 import './style/custom.scss';
 
 
