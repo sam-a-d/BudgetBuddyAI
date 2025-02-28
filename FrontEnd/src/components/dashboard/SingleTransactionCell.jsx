4002cb82d16aa0f2f1d866bc50c6b6986dbcbe5a
sam-a-d <masamad.sust@gmail.com>
2025-02-28 19:15:31 +0200

    aded single transaction cell

---
+++ FrontEnd/src/components/dashboard/SingleTransactionCell.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class SingleTransactionCell extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row align-items-start">
+                      <div className="col">
+                        <h6 className="mb-0">{this.props.description}</h6>
+                        <small className="">{this.props.time}</small>
+                      </div>
+                      <div className="col-auto">
+                        <h6 className="mb-0">
+                          {this.props.amount}
+                          {this.props.type == "Credit" ? (
+                            <span className="ms-2 align-top avtar avtar-xxs bg-light-success">+</span>
+                          ) : (
+                            <span className="ms-2 align-top avtar avtar-xxs bg-light-danger"><i className="ti  text-danger"> - </i></span>
+                          )}
+                        </h6>
+                      </div>
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default SingleTransactionCell;
