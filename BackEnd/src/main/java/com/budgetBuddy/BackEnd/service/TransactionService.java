6066ec5a78acc946c7004bd64502262b6db66eeb
sam-a-d <masamad.sust@gmail.com>
2025-03-02 00:27:59 +0200

    aded transaction with filter method to filter out transaction

--- BackEnd/src/main/java/com/budgetBuddy/BackEnd/service/TransactionService.java
+++ BackEnd/src/main/java/com/budgetBuddy/BackEnd/service/TransactionService.java
@@ -7,6 +7,11 @@ import com.budgetBuddy.BackEnd.model.User;
 import com.budgetBuddy.BackEnd.repository.CategoryRepo;
 import com.budgetBuddy.BackEnd.repository.TransactionRepo;
 import com.budgetBuddy.BackEnd.repository.UserRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +20,9 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import java.util.ArrayList;
+
 
 
 @Service
@@ -28,6 +36,13 @@ public class TransactionService {
 
     @Autowired
     CategoryRepo categoryRepo;
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public TransactionService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     public ResponseEntity<?> addTransaction(TransactionDTO transactionDTO){
 
@@ -79,5 +94,49 @@ public class TransactionService {
 
     public List<Transaction> getAllTransaction() {
         return transactionRepo.findAll();
+    }
+
+    public ResponseEntity<?> getTransactionWithFilter(String startDate, String endDate,
+                                                      String type, Long user_id,
+                                                      Double minAmount, Double maxAmount)
+    {
+
+        User transactionUser;
+        TransactionType theTransactionType;
+
+        CriteriaBuilder  criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Transaction> criteriaQuery = criteriaBuilder.createQuery(Transaction.class);
+
+        Root<Transaction> root = criteriaQuery.from(Transaction.class);
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if( user_id != null){
+            try{
+                transactionUser = userRepo.findById(user_id)
+                        .orElseThrow(() -> new RuntimeException("Role is not valid"));
+
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error in User/Transaction Type");
+            }
+            predicates.add(criteriaBuilder.equal(root.get("user"), transactionUser));
+        }
+
+        if (type != null && !type.equalsIgnoreCase("all")){
+            try {
+                theTransactionType = TransactionType.valueOf(type);
+                predicates.add(criteriaBuilder.equal(root.get("transactionType"), theTransactionType));
+            }catch (Exception e){
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                        "Error with transaction type"
+                );
+            }
+        }
+
+        criteriaQuery.where(predicates.toArray(new Predicate[0]));
+
+        return ResponseEntity.ok(entityManager.createQuery(criteriaQuery).getResultList());
     }
 }
