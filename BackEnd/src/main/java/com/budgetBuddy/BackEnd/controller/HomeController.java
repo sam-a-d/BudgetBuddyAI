acfec1da6cccec5f592a205810be636a4113469c
sam-a-d <masamad.sust@gmail.com>
2025-03-02 00:29:10 +0200

    corrected the home controller url

--- BackEnd/src/main/java/com/budgetBuddy/BackEnd/controller/HomeController.java
+++ BackEnd/src/main/java/com/budgetBuddy/BackEnd/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/")
 public class HomeController {
 
     @PreAuthorize("PermitAll()")
