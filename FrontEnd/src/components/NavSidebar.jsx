bc8ca428360f488c8ca7db128e3116398a457359
sam-a-d <masamad.sust@gmail.com>
2025-03-05 01:28:43 +0200

    Replaced hard coaded menu with menu component

--- FrontEnd/src/components/NavSidebar.jsx
+++ FrontEnd/src/components/NavSidebar.jsx
@@ -1,44 +1,43 @@
+import React from 'react';
+import MenuItem from './microComponents/menuitem';
+
 const NavSidebar = () => {
-    return(
-      <nav className="pc-sidebar">
-      <div className="navbar-wrapper">
+      return (
+        <nav className="pc-sidebar">
+          <div className="navbar-wrapper">
         <div className="m-header">
           <a className="navbar-brand" href="/">BudgetBuddy</a>
         </div>
         <div className="navbar-content">
           <ul className="pc-navbar">
-            <li className="pc-item pc-caption">
-              <label>Dashboard</label>
-              <i className="ti ti-dashboard"></i>
+            <li className="pc-item">
+              <MenuItem href="#" text="Transactions" />
             </li>
             <li className="pc-item">
-              <a href="#" className="pc-link"
-                ><span className="pc-micon"><i className="ti ti-dashboard"></i></span><span className="pc-mtext">Default</span></a
-              >
-            </li>
-
-            <li className="pc-item">
-              <a className="pc-link" target="_blank" href="#">
-                <span className="pc-micon"><i className="ti ti-lock"></i></span>
-                <span className="pc-mtext">Login</span>
-              </a>
+              <MenuItem href="#" text="Budgets" />
             </li>
             <li className="pc-item">
-              <a href="#" target="_blank" className="pc-link">
-                <span className="pc-micon"><i className="ti ti-user-plus"></i></span>
-                <span className="pc-mtext">Register</span>
-              </a>
+              <MenuItem href="#" text="Reports & Insights" />
             </li>
-
+            <li className="pc-item">
+              <MenuItem href="#" text="Savings Goals" />
+            </li>
+            <li className="pc-item">
+              <MenuItem href="#" text="Debt Management" />
+            </li>
+            <li className="pc-item">
+              <MenuItem href="#" text="Investment Tracking" />
+            </li>
+            <li className="pc-item">
+              <MenuItem href="#" text="Settings &amp; Preferences" />
+            </li>
           </ul>
-
           <div className="w-100 text-center">
             <div className="badge theme-version badge rounded-pill bg-light text-dark f-12"></div>
           </div>
         </div>
-      </div>
-    </nav>
-    )
-}
-
+          </div>
+        </nav>
+      );
+    }
 export default NavSidebar;
